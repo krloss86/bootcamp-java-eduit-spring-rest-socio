78b5066c9e4dd100d1551725350ba1cd9fbfc641
carlos horacio lopez <krloss@gmail.com>
2022-09-23 19:42:19 -0300

    first commit: microservicio socios

---
+++ src/main/java/ar/com/educacionit/SpringRestSociosApplication.java
@@ -0,0 +1,13 @@
+package ar.com.educacionit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSociosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestSociosApplication.class, args);
+	}
+
+}
